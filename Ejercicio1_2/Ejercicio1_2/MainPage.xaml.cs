868c43165f976902d8da1074af75e2053f7cb49c
gjackelineeb <you@example.com>
2023-02-26 23:16:33 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_2/Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Models.Empleados
+            {
+                nombre = txtNombre.Text,
+                apellidos = txtApellidos.Text,
+                edad = txtEdad.Text,
+                email = txtEmail.Text,
+            };
+
+            var datos = new PageData();
+            datos.BindingContext = emple;
+            await Navigation.PushAsync(datos);
+
+        }
+    }
+}
---
+++ Ejercicio1_2/Ejercicio1_2/Models/Empleados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2.Models
+{
+    public class Empleados
+    {
+           public string nombre { get; set; }
+            public string apellidos { get; set; }
+            public string edad { get; set; }
+            public string email { get; set; }
+        
+    }
+}
